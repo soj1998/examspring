3607d5d21c6a33566bc36e6f878e52049bd9948f
soj1998 <736495034@qq.com>
2020-11-22 23:17:41 +0800

    1122

--- houtai/src/main/java/com/rm/MyTest.java
+++ houtai/src/main/java/com/rm/MyTest.java
@@ -10,20 +10,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class MyTest {
 	
 	@ResponseBody
-	 @RequestMapping("/hello")
-	    public String say(){
-	        return "Spring Boot 你大爷222！";
-	    }
-	 @RequestMapping("/say")
-	    public ModelAndView say2(){
-	        ModelAndView mav=new ModelAndView();
-	        mav.addObject("message", "SpringBoot 大爷你好！");
-	        mav.setViewName("hh.html");
-	        return mav;
-	    }
-	 @ResponseBody
-	 @RequestMapping("/say2")
-	    public String say3(){
-	        return "hh3";
-	    }
+	@RequestMapping("/hello")
+    public String say(){
+        return "Spring Boot 你大爷222！";
+    }
+	@RequestMapping("/say")
+    public ModelAndView say2(){
+        ModelAndView mav=new ModelAndView();
+        mav.addObject("message", "SpringBoot 大爷你好！");
+        mav.setViewName("hh.html");
+        return mav;
+    }
+	@ResponseBody
+	@RequestMapping("/say2")
+    public String say3(){
+        return "hh3";
+	}
 }
