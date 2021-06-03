dc8cdaa25ed0e2b57fd67aebb8be3b120c10d9b3
soj1998 <736495034@qq.com>
2021-06-03 16:53:34 +0800

    0603

--- houtai/src/main/java/com/rm/HoutaiApplication.java
+++ houtai/src/main/java/com/rm/HoutaiApplication.java
@@ -2,18 +2,13 @@ package com.rm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HoutaiApplication extends SpringBootServletInitializer{
+public class HoutaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoutaiApplication.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HoutaiApplication.class);
-	}
+	
 
 }
